e27da62ef3a84e1c85efb3b479c00584477a41df
jessikka12 <69516048+jessikka12@users.noreply.github.com>
2023-05-01 19:54:00 -0400

    removed mouth stuff, added blur background frontend

---
+++ Toggle.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+
+class Toggle(tk.Frame):
+    """Toggle widget which can toggle between on and off."""
+    value = False
+
+    def __init__(self, master, height=25, 
+                 background='#ffffff', foreground='#000000',
+                 troughcolor='#ff0000', highlightbackground='#0000ff'):
+        self.background = background
+        self.troughcolor = troughcolor
+        tk.Frame.__init__(self, master, height=height, width=100, background=highlightbackground)
+
+        self.widget = tk.Frame(self, background=background, height=height, width=50, borderwidth=3)
+
+        self.front = tk.Frame(self.widget, background=background, height=height, width=30, relief='raised', borderwidth=2)
+        self.back = tk.Label(self.widget, background=background, text='ON', foreground=foreground, width=3)
+
+        self.widget.bind('<Button-1>', lambda e:self.toggle())
+        self.front.bind('<Button-1>', lambda e:self.toggle())
+        self.back.bind('<Button-1>', lambda e:self.toggle())
+
+        self.front.grid(row=0, column=0)
+        self.back.grid(row=0, column=1)
+        self.widget.pack(padx=1, pady=1)
+        
+    def toggle(self):
+        if (self.value):
+            # turn off
+            self.front.grid(row=0, column=0)
+            self.value = False
+            self.back.config(text='ON', background=self.background)
+        else:
+            # turn on
+            self.front.grid(row=0, column=2)
+            self.value = True
+            self.back.config(text='', background=self.troughcolor, height=1)
+
+    def getValue(self):
+        return self.value
+    
+if __name__ == '__main__':
+    root = tk.Tk()
+    toggle = Toggle(root, height=22)
+    toggle.pack(padx=20, pady=20)
+    root.mainloop()
--- gui.py
+++ gui.py
@@ -4,19 +4,10 @@ from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
 import cv2
-from tkinter import filedialog
-from tkinter import messagebox
-import cv2
 
-from Filter import Filter
+from Toggle import Toggle
 from Filter import Filter
 
-class ImageAlignmentFrame(tk.Tk):
-      COL1 = '#FFF4E0'
-      COL2 = '#FFBF9B'
-      COL3 = '#B46060'
-      COL4 = '#4D4D4D'
-      SCALE_LENGTH = 200
 class ImageAlignmentFrame(tk.Tk):
       COL1 = '#FFF4E0'
       COL2 = '#FFBF9B'
@@ -40,11 +31,6 @@ class ImageAlignmentFrame(tk.Tk):
       mouthRaw = Image.open('assets/mouth.png')
       mouthSized = mouthRaw.resize((mW,mH))
       mouthRotated = mouthSized
-      mW = int(0.4*fW) # mouth width = 80
-      mH = int(0.15*fH) # mouthx2 height
-      mouthRaw = Image.open('assets/mouth.png')
-      mouthSized = mouthRaw.resize((mW,mH))
-      mouthRotated = mouthSized
 
       eS = int(.3*fW) # eye size (square) = 64
       leyeRaw = Image.open('assets/leye.png')
@@ -55,64 +41,14 @@ class ImageAlignmentFrame(tk.Tk):
       reyeSized = reyeRaw.resize((eS,eS))
       reyeRotated = reyeSized
 
-      iS = int(.1*imgH)
-      fileRaw = Image.open('assets/file3.png')
-      fileSized = fileRaw.resize((iS, iS))
+      # iS = int(.1*imgH)
+      # fileRaw = Image.open('assets/file3.png')
+      # fileSized = fileRaw.resize((iS, iS))
 
-      camRaw = Image.open('assets/cam3.png')
-      camSized = camRaw.resize((iS, iS))
-
-      def __init__(self):
-            super(ImageAlignmentFrame, self).__init__()
-            self.title('Title')
-            self.config(background=self.COL1)
-
-            # styling
-            s = ttk.Style()
-            s.theme_use('default')
-            s.configure('TLabel', background=self.COL1, foreground=self.COL4, padding=3)
-            s.configure('TFrame', background=self.COL1)
-            s.configure('TNotebook', background=self.COL2, borderwidth=0, padding=2)
-            s.configure('TNotebook.Tab', background=self.COL2, foreground=self.COL4, 
-                        borderwidth=0, padding=3)
-            s.map('TNotebook.Tab', background=[('selected',self.COL1), ('focus',self.COL1)], 
-                  foreground=[('selected',self.COL4)])
-
-
-            # create image
-            self.imgCvs = tk.Canvas(self, height=self.imgH, width=self.imgW,
-                              background=self.COL1, highlightbackground=self.COL2)
-
-            face = self.imgCvs.create_oval((self.imgW - self.fW)/2, (self.imgH - self.fH)/2,
-                                    (self.imgW + self.fW)/2, (self.imgH + self.fH)/2,
-                                    outline=self.COL4)
-
-            self.mouthImg = ImageTk.PhotoImage(self.mouthSized)
-            self.mouth = self.imgCvs.create_image(self.imgW/2, (self.imgH+self.fH/2)/2,
-                                    anchor='center', image=self.mouthImg)
-
-            self.leyeImg = ImageTk.PhotoImage(self.leyeSized)
-            self.leye = self.imgCvs.create_image((self.imgW - self.fW/2)/2, (self.imgH - self.fH/6)/2,
-                                    anchor='center',image=self.leyeImg)
-
-            self.reyeImg = ImageTk.PhotoImage(self.reyeSized)
-            self.reye = self.imgCvs.create_image((self.imgW + self.fW/2)/2, (self.imgH - self.fH/6)/2,
-                                    anchor='center',image=self.reyeImg)
-      eS = int(.3*fW) # eye size (square) = 64
-      leyeRaw = Image.open('assets/leye.png')
-      leyeSized = leyeRaw.resize((eS,eS))
-      leyeRotated = leyeSized
+      # camRaw = Image.open('assets/cam3.png')
+      # camSized = camRaw.resize((iS, iS))
       
-      reyeRaw = Image.open('assets/reye.png')
-      reyeSized = reyeRaw.resize((eS,eS))
-      reyeRotated = reyeSized
-
-      iS = int(.1*imgH)
-      fileRaw = Image.open('assets/file3.png')
-      fileSized = fileRaw.resize((iS, iS))
-
-      camRaw = Image.open('assets/cam3.png')
-      camSized = camRaw.resize((iS, iS))
+      blur = False
 
       def __init__(self):
             super(ImageAlignmentFrame, self).__init__()
@@ -154,24 +90,10 @@ class ImageAlignmentFrame(tk.Tk):
 
             # settings, tabs are mouth or eyes
             settingsTabs = ttk.Notebook(self)
-            # settings, tabs are mouth or eyes
-            settingsTabs = ttk.Notebook(self)
 
             # eyes tab
             eyesTabFrm = ttk.Frame(settingsTabs)
-            # eyes tab
-            eyesTabFrm = ttk.Frame(settingsTabs)
 
-            # choose eye rotation angle
-            eRotationFrm = ttk.Frame(eyesTabFrm)
-            eRotLbl = ttk.Label(eRotationFrm, text='Rotation')
-            eRotMinLbl = ttk.Label(eRotationFrm, text='-180°', foreground=self.COL3)
-            self.eRotScl = tk.Scale(eRotationFrm, orient='horizontal',
-                              background=self.COL1,
-                              highlightbackground=self.COL2, troughcolor=self.COL3,
-                              length=self.SCALE_LENGTH, from_=-180, to=180, showvalue=0,
-                              resolution=5, command=self.eyeRotate)
-            eRotMaxLbl = ttk.Label(eRotationFrm, text='180°', foreground=self.COL3)
             # choose eye rotation angle
             eRotationFrm = ttk.Frame(eyesTabFrm)
             eRotLbl = ttk.Label(eRotationFrm, text='Rotation')
@@ -204,120 +126,29 @@ class ImageAlignmentFrame(tk.Tk):
             eResMinLbl.grid(row=1, column=0)
             self.eResScl.grid(row=1, column=1)
             eResMaxLbl.grid(row=1, column=2)
-            eResLbl.grid(row=0, column=1, sticky='W')
-            eResMinLbl.grid(row=1, column=0)
-            self.eResScl.grid(row=1, column=1)
-            eResMaxLbl.grid(row=1, column=2)
 
             # place things in eyes tab
             eRotationFrm.pack(pady=10, padx=5)
             eResizeFrm.pack(pady=10, padx=5)
             settingsTabs.add(eyesTabFrm, text='eyes')
-            # place things in eyes tab
-            eRotationFrm.pack(pady=10, padx=5)
-            eResizeFrm.pack(pady=10, padx=5)
-            settingsTabs.add(eyesTabFrm, text='eyes')
 
+            # other tab
+            otherTabFrm = ttk.Frame(settingsTabs)
 
-            # mouth tab
-            mouthTabFrm = ttk.Frame(settingsTabs)
-            # mouth tab
-            mouthTabFrm = ttk.Frame(settingsTabs)
+            # blur background toggle
+            blurFrm = ttk.Frame(otherTabFrm)
+            self.blurTgl = Toggle(blurFrm, height=22,
+                                 background=self.COL1, foreground=self.COL3,
+                                 troughcolor=self.COL3, highlightbackground=self.COL2)
+            blurLbl = ttk.Label(blurFrm, text='Blur Background')
 
-            # choose mouth rotation angle
-            mRotationFrm = ttk.Frame(mouthTabFrm)
-            mRotLbl = ttk.Label(mRotationFrm, text='Rotation')
-            mRotMinLbl = ttk.Label(mRotationFrm, text='-180°', foreground=self.COL3)
-            self.mRotScl = tk.Scale(mRotationFrm, orient='horizontal', 
-                              background=self.COL1,
-                              highlightbackground=self.COL2, troughcolor=self.COL3,
-                              length=self.SCALE_LENGTH, from_=-180, to=180, showvalue=0,
-                              resolution=180, command=self.mouthRotate)
-            mRotMaxLbl = ttk.Label(mRotationFrm, text='180°', foreground=self.COL3)
-            # choose mouth rotation angle
-            mRotationFrm = ttk.Frame(mouthTabFrm)
-            mRotLbl = ttk.Label(mRotationFrm, text='Rotation')
-            mRotMinLbl = ttk.Label(mRotationFrm, text='-180°', foreground=self.COL3)
-            self.mRotScl = tk.Scale(mRotationFrm, orient='horizontal', 
-                              background=self.COL1,
-                              highlightbackground=self.COL2, troughcolor=self.COL3,
-                              length=self.SCALE_LENGTH, from_=-180, to=180, showvalue=0,
-                              resolution=180, command=self.mouthRotate)
-            mRotMaxLbl = ttk.Label(mRotationFrm, text='180°', foreground=self.COL3)
+            self.blurTgl.grid(row=0, column=0, padx=5)
+            blurLbl.grid(row=0, column=1, sticky='E')
 
-            mRotLbl.grid(row=0, column=1, sticky='W')
-            mRotMinLbl.grid(row=1, column=0)
-            self.mRotScl.grid(row=1, column=1)
-            mRotMaxLbl.grid(row=1, column=2)
-            mRotLbl.grid(row=0, column=1, sticky='W')
-            mRotMinLbl.grid(row=1, column=0)
-            self.mRotScl.grid(row=1, column=1)
-            mRotMaxLbl.grid(row=1, column=2)
+            # place things in other tab
+            blurFrm.pack(pady=10, padx=5)
+            settingsTabs.add(otherTabFrm, text='other')
 
-            # choose mouth resize
-            mResizeFrm = ttk.Frame(mouthTabFrm)
-            mResLbl = ttk.Label(mResizeFrm, text='Resize')
-            mResMinLbl = ttk.Label(mResizeFrm, text='100%', foreground=self.COL3)
-            self.mResScl = tk.Scale(mResizeFrm, orient='horizontal',
-                              background=self.COL1,
-                              highlightbackground=self.COL2, troughcolor=self.COL3,
-                              length=self.SCALE_LENGTH, from_=1, to=2, showvalue=0,
-                              resolution=1, command=self.mouthResize)
-
-            mResMaxLbl = ttk.Label(mResizeFrm, text='250%', foreground=self.COL3)
-
-            mResLbl.grid(row=0, column=1, sticky='W')
-            mResMinLbl.grid(row=1, column=0)
-            self.mResScl.grid(row=1, column=1)
-            mResMaxLbl.grid(row=1, column=2)
-            mResLbl.grid(row=0, column=1, sticky='W')
-            mResMinLbl.grid(row=1, column=0)
-            self.mResScl.grid(row=1, column=1)
-            mResMaxLbl.grid(row=1, column=2)
-
-            # place things in mouth tab
-            mRotationFrm.pack(pady=10, padx=5)
-            mResizeFrm.pack(pady=10, padx=5)
-            settingsTabs.add(mouthTabFrm, text='mouth')
-            # place things in mouth tab
-            mRotationFrm.pack(pady=10, padx=5)
-            mResizeFrm.pack(pady=10, padx=5)
-            settingsTabs.add(mouthTabFrm, text='mouth')
-
-            # # icons, file and webcam buttons
-            # iconsFrm = ttk.Frame(self)
-
-            # # file button
-            # self.fileImg = ImageTk.PhotoImage(self.fileSized)
-            # self.fileBtn = tk.Label(iconsFrm, image=self.fileImg,
-            #                    background=self.COL1)
-            # self.fileBtn.bind('<Button-1>', lambda e:self.getImagePath())            
-
-            # # cam button
-            # self.camImg = ImageTk.PhotoImage(self.camSized)
-            # camBtn = tk.Label(iconsFrm, image=self.camImg,
-            #                   background=self.COL1)
-            # camBtn.bind('<Button-1>', lambda e:self.openWebCam())
-
-            # self.fileBtn.grid(row=0, column=1)
-            # camBtn.grid(row=0, column=0)
-            # # icons, file and webcam buttons
-            # iconsFrm = ttk.Frame(self)
-
-            # # file button
-            # self.fileImg = ImageTk.PhotoImage(self.fileSized)
-            # self.fileBtn = tk.Label(iconsFrm, image=self.fileImg,
-            #                    background=self.COL1)
-            # self.fileBtn.bind('<Button-1>', lambda e:self.getImagePath())            
-
-            # # cam button
-            # self.camImg = ImageTk.PhotoImage(self.camSized)
-            # camBtn = tk.Label(iconsFrm, image=self.camImg,
-            #                   background=self.COL1)
-            # camBtn.bind('<Button-1>', lambda e:self.openWebCam())
-
-            # self.fileBtn.grid(row=0, column=1)
-            # camBtn.grid(row=0, column=0)
 
             # submit button
             submitBtn = tk.Label(self, text='submit', 
@@ -360,68 +191,52 @@ class ImageAlignmentFrame(tk.Tk):
             self.imgCvs.itemconfig(self.reye, image=self.reyeImg)
             # print(f'{eS} * {val} = {nEyeSize}')
 
-      def mouthRotate(self, val):
-            angle = int(val)
-            self.mouthRotated = self.mouthSized.rotate(angle)
-            self.mouthImg = ImageTk.PhotoImage(self.mouthRotated)
-            self.imgCvs.itemconfig(self.mouth, image=self.mouthImg)
-
-
-      def mouthResize(self, val):
-            nMouthW = int( float(val) * self.mW )
-            nMouthH = int( float(val) * self.mH )
-            self.mouthSized = self.mouthRaw.resize((nMouthW, nMouthH))
-            self.mouthResized = self.mouthRotated.resize((nMouthW, nMouthH))
-            self.mouthImg = ImageTk.PhotoImage(self.mouthResized)
-            self.imgCvs.itemconfig(self.mouth, image=self.mouthImg)
-
-      # def getImagePath(self):
-      #       self.image_path = tk.filedialog.askopenfilename()
-      #       # print(self.image_path)
-      #       try:
-      #             faceRaw = Image.open(self.image_path)
-      #             faceSized = faceRaw.resize((int(faceRaw.width * self.iS / faceRaw.height), self.iS))
-      #             self.fileImg = ImageTk.PhotoImage(faceSized)
-      #             self.fileBtn.config(image=self.fileImg)
-      #       except:
-      #             tk.messagebox.showerror('Image Error', 'Please make sure your image is an image.')
+      def toggleBlur(self):
+            print(f'toggle clicked {self.blur}')
+            if (self.blur):
+                  self.blurTgl.set(1)
+                  self.blur = False
+            else:
+                  self.blurTgl.set(2)
+                  self.blur = True
 
       def openWebCam(self):
             # print('ideally, this would open the webcam and do the things')
-            self.withdraw()
-            vid = cv2.VideoCapture(0)
-            while(True):
-                  ret, frame = vid.read()
-                  frame = frame[:,::-1]
+            try:
+                  self.withdraw()
+                  vid = cv2.VideoCapture(0)
+                  while(True):
+                        ret, frame = vid.read()
+                        frame = frame[:,::-1]
 
-                  face_filter = Filter(use_url=False, input_image=frame)
-                  face_filter.applyEyeFilter(int(self.eResScl.get()), int(self.eRotScl.get()))
+                        face_filter = Filter(use_url=False, input_image=frame)
+                        face_filter.applyEyeFilter(int(self.eResScl.get()), int(self.eRotScl.get()))
 
-                  new_frame = face_filter.modified_img
-            
+                        new_frame = face_filter.modified_img
+                  
 
-                  # display webcam
-                  cv2.imshow('PRESS Q TO EXIT', new_frame)
-                  if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
+                        # display webcam
+                        cv2.imshow('PRESS Q TO EXIT', new_frame)
+                        if cv2.waitKey(1) & 0xFF == ord('q'):
+                              break
 
-            # After the loop release the cap object
-            vid.release()
-            # Destroy all the windows
-            cv2.destroyAllWindows()
-            self.deiconify()
-
+                  # After the loop release the cap object
+                  vid.release()
+                  # Destroy all the windows
+                  cv2.destroyAllWindows()
+            except:
+                  print('webcam failed')
+                  tk.messagebox.showerror('Webcam Failure', "Webcam failed to open")
+            finally:
+                  self.deiconify()
+                  self.getSettings()
 
       # submit Button
       def getSettings(self):
             try:
-                  print(f'image path: ', self.image_path)
                   print(f'eye rotation: ', self.eRotScl.get())
                   print(f'eye resize: ', self.eResScl.get())
-                  print(f'mouth rotation: ', self.mRotScl.get())
-                  print(f'mouth resize: ', self.mResScl.get())
-            except:
-                  tk.messagebox.showerror('Image Error', "Don't forget your image!")
+                  print(f'blur background: ', self.blur)
             finally:
                   print('button clicked')
 
