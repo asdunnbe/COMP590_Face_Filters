261195b7b4681fc2e5063eeae2df7f397c145701
jessikka12 <69516048+jessikka12@users.noreply.github.com>
2023-05-04 14:21:22 -0400

    added toggle for sunglasses

--- gui.py
+++ gui.py
@@ -4,14 +4,10 @@ from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
 import cv2
-<<<<<<< HEAD
 from datetime import datetime
 import os
 
 from Toggle import Toggle
-=======
-
->>>>>>> dd190ce... webcam linked up
 from Filter import Filter
 from remove_background import MPSegmentation
 
@@ -22,13 +18,8 @@ class ImageAlignmentFrame(tk.Tk):
       COL4 = '#4D4D4D'
       SCALE_LENGTH = 200
 
-      imgH = 300
-      imgW = 300
-      
-      fW = 2/3 * imgW # face width = 200
-      fH = 1.2*fW # face height
-      imgH = 300
-      imgW = 300
+      imgH = 350
+      imgW = 350
       
       fW = 2/3 * imgW # face width = 200
       fH = 1.2*fW # face height
@@ -44,6 +35,11 @@ class ImageAlignmentFrame(tk.Tk):
       reyeRaw = Image.open('assets/reye.png')
       reyeSized = reyeRaw.resize((eS,eS))
       reyeRotated = reyeSized
+
+      gW = int((imgW - fW/2)/2 + 2*eS)
+      gH = int(1.5*eS)
+      glassesRaw = Image.open('assets/glasses.png')
+      glassesSized = glassesRaw.resize((gW,gH))
       
       blur = False
 
@@ -94,23 +90,37 @@ class ImageAlignmentFrame(tk.Tk):
             self.reye = self.imgCvs.create_image((self.imgW + self.fW/2)/2, (self.imgH - self.fH/6)/2,
                                     anchor='center',image=self.reyeImg)
 
+            self.glassesImg = ImageTk.PhotoImage(self.glassesSized)
+            self.glasses = self.imgCvs.create_image((self.imgW)/2, (self.imgH - self.fH/6)/2,
+                                                    anchor='center',image=self.glassesImg,state='hidden')
 
             # settings, tabs are mouth or eyes
-            settingsTabs = ttk.Notebook(self)
+            self.settingsTabs = ttk.Notebook(self, height=200, width=350)
 
             # eyes tab
-            eyesTabFrm = ttk.Frame(settingsTabs)
+            eyesTabFrm = ttk.Frame(self.settingsTabs)
+
+            # toggle sunglasses
+            glassesFrm = ttk.Frame(eyesTabFrm)
+            self.glassesTgl = Toggle(glassesFrm, height=22,
+                        background=self.COL1, foreground=self.COL3,
+                        troughcolor=self.COL3, highlightbackground=self.COL2,
+                        command=self.toggleGlasses)
+            glassesLbl = ttk.Label(glassesFrm, text='Sunglasses')
+
+            self.glassesTgl.grid(row=0, column=0)
+            glassesLbl.grid(row=0, column=1)
 
             # choose eye rotation angle
-            eRotationFrm = ttk.Frame(eyesTabFrm)
-            eRotLbl = ttk.Label(eRotationFrm, text='Rotation')
-            eRotMinLbl = ttk.Label(eRotationFrm, text='-180°', foreground=self.COL3)
-            self.eRotScl = tk.Scale(eRotationFrm, orient='horizontal',
+            self.eRotationFrm = ttk.Frame(eyesTabFrm)
+            eRotLbl = ttk.Label(self.eRotationFrm, text='Rotation')
+            eRotMinLbl = ttk.Label(self.eRotationFrm, text='-180°', foreground=self.COL3)
+            self.eRotScl = tk.Scale(self.eRotationFrm, orient='horizontal',
                               background=self.COL1,
                               highlightbackground=self.COL2, troughcolor=self.COL3,
                               length=self.SCALE_LENGTH, from_=-180, to=180, showvalue=0,
                               resolution=5, command=self.eyeRotate)
-            eRotMaxLbl = ttk.Label(eRotationFrm, text='180°', foreground=self.COL3)
+            eRotMaxLbl = ttk.Label(self.eRotationFrm, text='180°', foreground=self.COL3)
 
             eRotLbl.grid(row=0, column=1, sticky='W')
             eRotMinLbl.grid(row=1, column=0)
@@ -118,29 +128,30 @@ class ImageAlignmentFrame(tk.Tk):
             eRotMaxLbl.grid(row=1, column=2)
 
             # choose eye resize
-            eResizeFrm = ttk.Frame(eyesTabFrm)
-            eResLbl = ttk.Label(eResizeFrm, text='Resize')
-            eResMinLbl = ttk.Label(eResizeFrm, text='100%', foreground=self.COL3)
-            self.eResScl = tk.Scale(eResizeFrm, orient='horizontal',
+            self.eResizeFrm = ttk.Frame(eyesTabFrm)
+            eResLbl = ttk.Label(self.eResizeFrm, text='Resize')
+            eResMinLbl = ttk.Label(self.eResizeFrm, text='100%', foreground=self.COL3)
+            self.eResScl = tk.Scale(self.eResizeFrm, orient='horizontal',
                               background=self.COL1,
                               highlightbackground=self.COL2, troughcolor=self.COL3,
                               length=self.SCALE_LENGTH, from_=1, to=2, showvalue=0,
                               resolution=1, command=self.eyeResize)
 
-            eResMaxLbl = ttk.Label(eResizeFrm, text='200%', foreground=self.COL3)
+            eResMaxLbl = ttk.Label(self.eResizeFrm, text='200%', foreground=self.COL3)
 
             eResLbl.grid(row=0, column=1, sticky='W')
             eResMinLbl.grid(row=1, column=0)
             self.eResScl.grid(row=1, column=1)
             eResMaxLbl.grid(row=1, column=2)
 
-            # place things in eyes tab
-            eRotationFrm.pack(pady=10, padx=5)
-            eResizeFrm.pack(pady=10, padx=5)
-            settingsTabs.add(eyesTabFrm, text='eyes')
+            # place things in eyes tab)
+            glassesFrm.pack(pady=10, padx=5)
+            self.eRotationFrm.pack(pady=10, padx=5)
+            self.eResizeFrm.pack(pady=10, padx=5)
+            self.settingsTabs.add(eyesTabFrm, text='eyes')
 
             # other tab
-            otherTabFrm = ttk.Frame(settingsTabs)
+            otherTabFrm = ttk.Frame(self.settingsTabs)
 
             # blur background toggle
             blurFrm = ttk.Frame(otherTabFrm)
@@ -154,65 +165,7 @@ class ImageAlignmentFrame(tk.Tk):
 
             # place things in other tab
             blurFrm.pack(pady=10, padx=5)
-            settingsTabs.add(otherTabFrm, text='other')
-
-<<<<<<< HEAD
-=======
-            # choose mouth rotation angle
-            mRotationFrm = ttk.Frame(mouthTabFrm)
-            mRotLbl = ttk.Label(mRotationFrm, text='Rotation')
-            mRotMinLbl = ttk.Label(mRotationFrm, text='-180°', foreground=self.COL3)
-            self.mRotScl = tk.Scale(mRotationFrm, orient='horizontal', 
-                              background=self.COL1,
-                              highlightbackground=self.COL2, troughcolor=self.COL3,
-                              length=self.SCALE_LENGTH, from_=-180, to=180, showvalue=0,
-                              resolution=180, command=self.mouthRotate)
-            mRotMaxLbl = ttk.Label(mRotationFrm, text='180°', foreground=self.COL3)
-
-            mRotLbl.grid(row=0, column=1, sticky='W')
-            mRotMinLbl.grid(row=1, column=0)
-            self.mRotScl.grid(row=1, column=1)
-            mRotMaxLbl.grid(row=1, column=2)
-
-            # choose mouth resize
-            mResizeFrm = ttk.Frame(mouthTabFrm)
-            mResLbl = ttk.Label(mResizeFrm, text='Resize')
-            mResMinLbl = ttk.Label(mResizeFrm, text='100%', foreground=self.COL3)
-            self.mResScl = tk.Scale(mResizeFrm, orient='horizontal',
-                              background=self.COL1,
-                              highlightbackground=self.COL2, troughcolor=self.COL3,
-                              length=self.SCALE_LENGTH, from_=1.0, to=2.5, showvalue=0,
-                              resolution=0.05, command=self.mouthResize)
-            mResMaxLbl = ttk.Label(mResizeFrm, text='250%', foreground=self.COL3)
-
-            mResLbl.grid(row=0, column=1, sticky='W')
-            mResMinLbl.grid(row=1, column=0)
-            self.mResScl.grid(row=1, column=1)
-            mResMaxLbl.grid(row=1, column=2)
-
-            # place things in mouth tab
-            mRotationFrm.pack(pady=10, padx=5)
-            mResizeFrm.pack(pady=10, padx=5)
-            settingsTabs.add(mouthTabFrm, text='mouth')
-
-            # # icons, file and webcam buttons
-            # iconsFrm = ttk.Frame(self)
-
-            # # file button
-            # self.fileImg = ImageTk.PhotoImage(self.fileSized)
-            # self.fileBtn = tk.Label(iconsFrm, image=self.fileImg,
-            #                    background=self.COL1)
-            # self.fileBtn.bind('<Button-1>', lambda e:self.getImagePath())            
-
-            # # cam button
-            # self.camImg = ImageTk.PhotoImage(self.camSized)
-            # camBtn = tk.Label(iconsFrm, image=self.camImg,
-            #                   background=self.COL1)
-            # camBtn.bind('<Button-1>', lambda e:self.openWebCam())
-
-            # self.fileBtn.grid(row=0, column=1)
-            # camBtn.grid(row=0, column=0)
->>>>>>> dd190ce... webcam linked up
+            self.settingsTabs.add(otherTabFrm, text='other')
 
             # submit button
             submitBtn = tk.Label(self, text='submit', 
@@ -223,12 +176,31 @@ class ImageAlignmentFrame(tk.Tk):
             # grid
             self.imgCvs.grid(row=0, column=1, rowspan=5, 
                         sticky='NW', padx=10, pady=10)
-            settingsTabs.grid(row=0, column=2, columnspan=3, 
+            self.settingsTabs.grid(row=0, column=2, columnspan=3, 
                         sticky='N', padx=10, pady=10)
-            # iconsFrm.grid(row=1, column=2,
-            #             sticky='NW', padx=10)
             submitBtn.grid(row=4, column=3,
                         sticky='S', padx=3, pady=10)
+
+      def toggleGlasses(self):
+            value = self.glassesTgl.getValue()
+            if (value):
+                  # hide rotate and resize frame
+                  self.eRotationFrm.pack_forget()
+                  self.eResizeFrm.pack_forget()
+                  self.settingsTabs.configure(height=100)
+                  # show sunglasses on the cartoon face
+                  self.imgCvs.itemconfig(self.glasses, state='normal')
+                  self.imgCvs.itemconfig(self.leye, state='hidden')
+                  self.imgCvs.itemconfig(self.reye, state='hidden')
+            else:
+                  # show rotate and resize frame
+                  self.eRotationFrm.pack(pady=10, padx=5)
+                  self.eResizeFrm.pack(pady=10, padx=5)
+                  self.settingsTabs.configure(height=200)
+                  # hide sunglasses on the cartoon face
+                  self.imgCvs.itemconfig(self.glasses, state='hidden')
+                  self.imgCvs.itemconfig(self.leye, state='normal')
+                  self.imgCvs.itemconfig(self.reye, state='normal')
 
       def eyeRotate(self, val):
             angle = int(val)
@@ -255,15 +227,6 @@ class ImageAlignmentFrame(tk.Tk):
             self.imgCvs.itemconfig(self.reye, image=self.reyeImg)
             # print(f'{eS} * {val} = {nEyeSize}')
 
-<<<<<<< HEAD
-      def toggleBlur(self):
-            print(f'toggle clicked {self.blur}')
-            if (self.blur):
-                  self.blurTgl.set(1)
-                  self.blur = False
-            else:
-                  self.blurTgl.set(2)
-                  self.blur = True
 
       def openWebCam(self):
             self.getSettings()
@@ -312,66 +275,15 @@ class ImageAlignmentFrame(tk.Tk):
                   cv2.destroyAllWindows()
                   # reopen gui
                   self.deiconify()
-=======
-      def mouthRotate(self, val):
-            angle = int(val)
-            self.mouthRotated = self.mouthSized.rotate(angle)
-            self.mouthImg = ImageTk.PhotoImage(self.mouthRotated)
-            self.imgCvs.itemconfig(self.mouth, image=self.mouthImg)
-
-
-      def mouthResize(self, val):
-            nMouthW = int( float(val) * self.mW )
-            nMouthH = int( float(val) * self.mH )
-            self.mouthSized = self.mouthRaw.resize((nMouthW, nMouthH))
-            self.mouthResized = self.mouthRotated.resize((nMouthW, nMouthH))
-            self.mouthImg = ImageTk.PhotoImage(self.mouthResized)
-            self.imgCvs.itemconfig(self.mouth, image=self.mouthImg)
-
-      # def getImagePath(self):
-      #       self.image_path = tk.filedialog.askopenfilename()
-      #       # print(self.image_path)
-      #       try:
-      #             faceRaw = Image.open(self.image_path)
-      #             faceSized = faceRaw.resize((int(faceRaw.width * self.iS / faceRaw.height), self.iS))
-      #             self.fileImg = ImageTk.PhotoImage(faceSized)
-      #             self.fileBtn.config(image=self.fileImg)
-      #       except:
-      #             tk.messagebox.showerror('Image Error', 'Please make sure your image is an image.')
-
-      def openWebCam(self):
-            # print('ideally, this would open the webcam and do the things')
-            # self.withdraw()
-            vid = cv2.VideoCapture(0)
-            while(True):
-                  ret, frame = vid.read()
-                  frame = frame[:,::-1]
-
-                  face_filter = Filter(use_url=False, input_image=frame)
-                  face_filter.applyEyeFilter(self.eResScl.get(), self.eRotScl.get())
-
-                  new_frame = face_filter.modified_img
-            
-
-                  # display webcam
-                  cv2.imshow('PRESS Q TO EXIT', new_frame)
-                  if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
-
-            # After the loop release the cap object
-            vid.release()
-            # Destroy all the windows
-            cv2.destroyAllWindows()
-            # self.deiconify()
->>>>>>> dd190ce... webcam linked up
 
       # submit Button
       def getSettings(self):
             try:
+                  print(f'blur background: ', self.blurTgl.getValue())
+                  print(f'sunglasses: ', self.glassesTgl.getValue())
                   print(f'eye rotation: ', self.eRotScl.get())
                   print(f'eye resize: ', self.eResScl.get())
-                  print(f'blur background: ', self.blur)
-            finally:
+            except:
                   print('button clicked')
 
 if __name__ == '__main__':
